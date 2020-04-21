63979885ceb5034489c398e60c8a4819d81bc03c
Kevin <seniorpepex@gmail.com>
2020-04-20 23:38:15 -0700

    Add test for finding an id that shouldn't exist

--- tests/test_chr_init_param.py
+++ tests/test_chr_init_param.py
@@ -1,5 +1,15 @@
 import chr_init_param
 import unittest
 
+class find_chr_by_id_tests(unittest.TestCase):
+    def setUp(self):
+        self.find_chr_by_id_instance = chr_init_param.find_chr_by_id
+    def tearDown(self):
+        return super().tearDown()
+class find_nonexistent_char():
+    def runTest(self):
+        assert self.find_chr_by_id_instance('B') == False, \
+            "chr id's should be numbers"
+
 if __name__ == "__main__":
     unittest.main()
